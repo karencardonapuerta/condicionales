b6628a42ffb5de0a211841640e85b2694f55c0ec
“amonsalv” <anamonsalve96@gmail.com>
2023-08-22 07:19:31 -0500

    feat: se ejemplifica el uso del condicional den python

---
+++ ciclos.py
@@ -0,0 +1,11 @@
+nombreUsuario = input('Digite su nombre: ')
+
+edadUsuario= int (input('digite su edad: ')) #casteo de datos, convertir un dato en otro
+
+if (edadUsuario >=18):
+    print ("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")  
+    print("usted no puede entrara la discoteca") 
+         
+print("continua el programa")    
---
+++ hidroituango.py
@@ -0,0 +1,12 @@
+nivelAngua = int (input("Ingrese el nivel del agua: "))
+
+# evaluando multiples condiciones
+
+if (nivelAngua > 0 and nivelAngua <= 200):
+    print(f"el nivel de agua es: {nivelAngua}, esta bajo")
+elif(nivelAngua > 200 and nivelAngua <= 400):  
+     print(f"el nivel de agua es: {nivelAngua}, esta normal")
+elif(nivelAngua > 400 ):  
+     print(f"el nivel de agua es: {nivelAngua}, esta alto, inicie evacuacion")
+else:
+    print(f"porfavor revise el nivel del agua")
